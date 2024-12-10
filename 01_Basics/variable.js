6ff5c8d118179d5df0b5a42c9ad469bc1bbaf27a
Saurav7985 <149267722+Saurav7985@users.noreply.github.com>
2024-12-10 13:19:03 +0000

    jj

--- 01_Basics/variable.js
+++ 01_Basics/variable.js
@@ -1,1 +1,12 @@
-console.log("saurav ; ) ")
+console.log("saurav ; ) ");
+
+const Accountid = 1223;
+let AccountEmail = "sauravraikwar26@gmail.com";
+var Accountpassword = "12356"
+accountcity = "hydrabad"
+
+// Accountid = 1246; not allowed
+
+console.log(Accountid);
+/*dont use var bcz of issue in block scope and functional scope*/
+console.table([Accountid ,AccountEmail ,Accountpassword, accountcity]);
