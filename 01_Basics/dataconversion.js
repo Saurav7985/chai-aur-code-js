543c8c8909f08c34f2ee73a9f1665ab96f2aee82
Saurav7985 <149267722+Saurav7985@users.noreply.github.com>
2024-12-10 15:55:24 +0000

    dataconversion

---
+++ 01_Basics/dataconversion.js
@@ -0,0 +1,18 @@
+let score = "33";
+
+console.log(typeof score);
+let valueinnumber = Number(score);
+console.log(typeof valueinnumber);
+console.log( score);
+
+let is_logged = 1;
+console.log(typeof is_logged)
+let booleanloggedin = Boolean(is_logged);
+console.log(typeof booleanloggedin)
+console.log(is_logged)
+
+let somenumb = 33;
+let stringnumber = String(somenumb)
+
+console.log(stringnumber);
+console.log(typeof stringnumber);
