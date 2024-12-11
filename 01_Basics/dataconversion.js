7ce8c007da8bd1d942cff6040cc4d32a74d30646
Saurav7985 <149267722+Saurav7985@users.noreply.github.com>
2024-12-11 13:08:35 +0000

    2

--- 01_Basics/dataconversion.js
+++ 01_Basics/dataconversion.js
@@ -1,18 +1,46 @@
 let score = "33";
 
-console.log(typeof score);
+// console.log(typeof score);
 let valueinnumber = Number(score);
-console.log(typeof valueinnumber);
-console.log( score);
+// console.log(typeof valueinnumber);
+// console.log( score);
 
 let is_logged = 1;
-console.log(typeof is_logged)
+// console.log(typeof is_logged)
 let booleanloggedin = Boolean(is_logged);
-console.log(typeof booleanloggedin)
-console.log(is_logged)
+// console.log(typeof booleanloggedin)
+// console.log(is_logged)
 
 let somenumb = 33;
 let stringnumber = String(somenumb)
 
-console.log(stringnumber);
-console.log(typeof stringnumber);
+// console.log(stringnumber);
+// console.log(typeof stringnumber);
+
+// ****************************************************operator**************************************
+
+let value = 3;
+let negvalue = -value;
+console.log(negvalue);
+
+console.log(2+2)
+console.log(2-2)
+console.log(2*2)
+console.log(2/2)
+console.log(2**3)
+console.log(2%2)
+
+let str1 = "hello";
+let str2 = "saurav";
+
+console.log(str1+str2);
+
+console.log("1" + 2)
+console.log(1 + "2")
+console.log("1"+2 + 2)
+console.log(1 + 2 + "2")
+console.log(+true)
+console.log(+"")
+console.log(2+2)
+
+
