120c90d69d122655b66df1f9ebf5e55ecb3cd091
Saurav7985 <149267722+Saurav7985@users.noreply.github.com>
2024-12-12 10:39:35 +0000

    2nd fool

--- datatypesummery.js
+++ datatypesummery.js
@@ -2,9 +2,15 @@
 
 //  7 types : String, Number, Boolearn, null, undefined, Symbol, BigInt
 
+
+//Non-primitive
+
+// Array
+// function
+// Objects
+
 const score = 100
 const scoreValue = 100.3
-
 const isLoggedIn = false
 const outsideTemp = null
 let userEmail;
@@ -14,8 +20,7 @@ const anotherId = Symbol('123')
 
 console.log(id === anotherId);
 
-// const bigNumber = 3456543576654356754n
-
+const bigNumber = 3456543576654356754n
 
 
 // Reference (Non primitive)
@@ -23,8 +28,9 @@ console.log(id === anotherId);
 // Array, Objects, Functions
 
 const heros = ["shaktiman", "naagraj", "doga"];
+
 let myObj = {
-    name: "hitesh",
+    name: "saurav",
     age: 22,
 }
 
@@ -32,4 +38,16 @@ const myFunction = function(){
     console.log("Hello world");
 }
 
-console.log(typeof anotherId);
+console.log("Type of int value : " + typeof score);
+console.log("Type of decimal value : " + typeof scoreValue);
+console.log("Type of boolean value : " + typeof isLoggedIn);
+console.log("Type of null value : " + typeof outsideTemp);
+console.log("Type of undefined value : " + typeof userEmail);
+console.log("Type of symbol value : " + typeof id);
+console.log("Type of 2nd symbol value : " + typeof anotherId);
+console.log("Type of big number value : " + typeof bigNumber);
+
+console.log("Type of Array value : " + typeof heros);
+console.log("Type of object value : " + typeof myObj);
+console.log("Type of function value : " + typeof myFunction);
+
