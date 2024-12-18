2c16db62aa7abacde60fe32779ad9d29a200b627
saurav <sauravraikwar26@gmail.com>
2024-12-18 20:33:23 +0530

    new commit

---
+++ 01_Basics/data_type.js
@@ -0,0 +1,26 @@
+"use strict";
+
+// alert(3+3); // work in browser and now we use node.js 
+
+console.log(3+3) ; console.log("hitesh"); // code is correct but code readibility is 0;
+
+let name = "saurav"
+let age = 17;
+let isturue = false
+
+// Number => 2 to power 53
+//string => " "
+// BigInt
+// Boolean =>ture/false
+// null =>standalone
+// undefined =>
+// Symbol =>unique
+
+// Object
+
+console.log(name)
+
+console.log(typeof name);
+console.table(typeof age);
+console.log(typeof istrue);
+
