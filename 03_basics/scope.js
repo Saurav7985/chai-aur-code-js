f4363e72ed6661ab19a93dbc91da43713769527c
Saurav7985 <149267722+Saurav7985@users.noreply.github.com>
2025-03-06 17:25:09 +0000

    scope

--- 03_basics/scope.js
+++ 03_basics/scope.js
@@ -8,12 +8,57 @@ if(true){
     let a = 10
     const b = 20
      var c = 30 //it has scope outside the if also.
-     console.log("inner : ",a);
+    //  console.log("inner : ",a);
      
 }
 
 
 
-console.log(a);
+// console.log(a);
 // console.log(b);
 // console.log(c);
+
+
+function one(){
+    const username = "hitesh"
+
+    function two(){
+        const website = "youtube"
+        // console.log(username);
+        
+    }
+    // console.log(website);
+    two();
+    
+}
+
+// one()
+
+if(true){
+
+    const username = "hitesh"
+    if(username == "hitesh"){
+        const website = " youtube"
+        // console.log(username + website)
+    }
+    // console.log(website);
+    
+}
+// console.log(username); // dont print bcz of  the scope dont allow it
+
+//++++++++++++++++++++++++++++++++++ interesting ++++++++++++++++++++++++++++++++
+
+console.log(addone(6));
+
+function addone(value){
+    return value + 1
+}
+
+// console.log(addtwo(6)); // but they dont acces before the declairation beacause of the const .
+
+const addtwo = function(num){
+    return num + 2
+}
+
+console.log(addtwo(6));
+
