4d2c45adaf5f82d74c7efafe16eca12bf9fcadec
Saurav7985 <149267722+Saurav7985@users.noreply.github.com>
2025-03-29 19:03:40 +0000

    complete

---
+++ 05_iterations/five.js
@@ -0,0 +1,38 @@
+const array = ["js","ruby","python","cpp"]
+
+array.forEach((element) => {
+    // console.log(element);
+});
+
+function printme(item){
+    console.log(item);
+    
+}
+
+// array.forEach(printme)
+
+array.forEach((item,index,arr) => {
+ //for each have these 3 things in it.
+    // console.log(item,index,arr);
+});
+
+const mylanguage =[
+
+{
+    languagename : "js",
+    languagefilename : "javascript"
+},
+{
+    languagename : "ry",
+    languagefilename : "rubby"
+},
+{
+    languagename : "py",
+    languagefilename : "python"
+}
+]
+
+mylanguage.forEach( (item) => {
+console.log(item.languagename);
+    }
+)
