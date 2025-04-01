97407a1114b78d8381542765fb6107b3ea51eb9f
Saurav7985 <149267722+Saurav7985@users.noreply.github.com>
2025-04-01 11:30:19 +0000

    save

--- 05_iterations/six.js
+++ 05_iterations/six.js
@@ -25,9 +25,21 @@ mynums.forEach(num => {
 // console.log(NewNumbs); //we also work with it like this
 
 const books = [
-    {title : "Book One" , genre : 'history' , publish: 1979 , edition : 1990}
+    {title : "Book One" , genre : 'history' , publish: 1979 , edition : 1998},
+    {title : "Book Two" , genre : 'Action' , publish: 1976 , edition : 1997},
+    {title : "Book Three" , genre : 'Comedy' , publish: 1956 , edition : 1994},
+    {title : "Book Four" , genre : 'Comedy' , publish: 1996 , edition : 1999},
+    {title : "Book Five" , genre : 'Horror' , publish: 1915 , edition : 1993},
+    {title : "Book Six" , genre : 'Comedy' , publish: 1977 , edition : 1994},
+    {title : "Book Seven" , genre : 'history' , publish: 1979 , edition : 1992},
+    {title : "Book Eight" , genre : 'Comedy' , publish: 1989 , edition : 1993}
 ]
 
+let userbook = books.filter((bk) => bk.genre == 'history')
+
+ userbook = books.filter((bk) => {return bk.publish <= 2000})
+
+console.log(userbook);
 
 
 
