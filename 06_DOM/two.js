1f3280a3b2759ecca585a66f7fc3f31823adf452
Saurav7985 <149267722+Saurav7985@users.noreply.github.com>
2025-04-09 11:28:38 +0000

    All

---
+++ 06_DOM/two.js
@@ -0,0 +1,9 @@
+// let ans = document.getElementsByClassName("card");
+
+// for (let element of ans) {
+//     element.style.backgroundColor = "green";
+//     margin = "12px";
+// padding = "12px";
+
+// }
+
